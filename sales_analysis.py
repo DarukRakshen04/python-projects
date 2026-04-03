461adc10de90d1f5873914b012d4656fa09ff184
DARUK RAKSHEN S <darukrakshen2004@gmail.com>
2026-04-03 15:19:57 +0530

    Update sales_analysis.py

--- sales_analysis.py
+++ sales_analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+# sample data
 data = {
     "product": ["A", "B", "A", "C"],
     "price": [100, 200, 100, 300],
@@ -8,7 +10,16 @@ data = {
 
 df = pd.DataFrame(data)
 
+# total column
 df["total"] = df["price"] * df["quantity"]
 
 print("Total Revenue:", df["total"].sum())
-print(df.groupby("product")["total"].sum())
+
+# group by product
+product_sales = df.groupby("product")["total"].sum()
+print(product_sales)
+
+# plot
+product_sales.plot(kind='bar')
+plt.title("Sales by Product")
+plt.show()
